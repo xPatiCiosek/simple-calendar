e31e09ed1e4896002cb86a9912fda587426aacce
xPatiCiosek <patrycja_ciosek@hotmail.com>
2024-02-09 23:42:07 +0000

    calendar layout

---
+++ script.js
@@ -0,0 +1,42 @@
+let nav =0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+
+const weekdays = ['Monday', 'Teusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const calendar = document.getElementById('calendar');
+
+function load(){
+    const dt = new Date();
+
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month+1, 0).getDate();
+
+    const dateString = firstDayOfMonth.toLocaleDateString('en-GB',{
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    });
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+    for(let i=1; i<= paddingDays + daysInMonth; i++){
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+
+        if(i> paddingDays){
+            daySquare.innerText = i - paddingDays;
+
+            daySquare.addEventListener('click', () => console.log('click'));
+        } else {
+            daySquare.classList.add('padding');
+        }
+
+        calendar.appendChild(daySquare);
+    }
+    
+}
+load();
